646093ce66da23201b5297d8a7589eece052d8c9
karenwsit <karenwsit@gmail.com>
2015-12-20 10:02:12 -0800

    added import os

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def index():
 PORT = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=PORT)
